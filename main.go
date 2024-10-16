99a0780f7094bb34df198bd7d8cfa91e53dc12de
Roey Zalta <roey.zalta@gmail.com>
2024-10-16 17:07:30 +0300

    reasure json keys are lower case

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"example.com/note/note"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	title, content := getNoteData()
+
+	userNote, err := note.New(title, content)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	userNote.Display()
+
+	err = userNote.Save()
+	if err != nil {
+		fmt.Println("Saving the note failed:", err)
+		return
+	}
+	fmt.Println("Note saved successfully!")
+
+}
+
+func getNoteData() (string, string) {
+	title := getUserInput("Note Title: ")
+	content := getUserInput("Note Content: ")
+	return title, content
+}
+
+func getUserInput(prompt string) string {
+	fmt.Println(prompt)
+	reader := bufio.NewReader(os.Stdin)
+
+	text, err := reader.ReadString('\n')
+	if err != nil {
+		return ""
+	}
+
+	text = strings.TrimSuffix(text, "\n")
+	text = strings.TrimSuffix(text, "\r")
+
+	return text
+}
